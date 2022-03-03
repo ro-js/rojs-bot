7bae0fa29ef9bf9a2d69a3c89c5c4602282d556f
Vlada <81084648+zlures@users.noreply.github.com>
2022-03-03 15:56:29 +0200

    Update getuser.command.js

--- app/commands/getuser.command.js
+++ app/commands/getuser.command.js
@@ -1,14 +1,32 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
 const rojs = require('@zlures/rojs');
-
+const {
+    MessageEmbed
+} = require('discord.js')
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('getuser')
-		.setDescription('Replies with an username from ID')
-	  .addNumberOption(option => option.setName('num').setDescription('ID of user'))
-    const number = interaction.options.getNumber('num');
-    function call(user) {
-      return user
-    }
-    interaction.reply(rojs.GetUsernameFromID(number, call));
+    data: new SlashCommandBuilder()
+        .setName('getuserfromid')
+        .addNumberOption(option => option.setName('id').setDescription('An UserID of user in Roblox.'))
+        .setDescription('Get username from ID!'),
+    async execute(interaction) {
+        const number = interaction.options.getNumber('id');
+
+        function call(request) {
+            const exampleEmbed = new MessageEmbed()
+            exampleEmbed.setColor('#deff98')
+            exampleEmbed.setTitle('Response')
+            exampleEmbed.setDescription('**Code**\n```const rojs = require("@zlures/rojs");\nrojs.GetUsernameByID(' + number + ', callback);```')
+            exampleEmbed.addFields({
+                name: 'Username',
+                value: request
+            }, )
+            interaction.reply({
+                embeds: [exampleEmbed],
+                ephemeral: false
+            })
+        }
+        rojs.GetUsernameByID(number, call);
+    },
 };
